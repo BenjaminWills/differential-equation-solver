dd9e1effde88544a10dfbd32dd9e90fb328f8341
BenjaminWills <benjaminwills047@gmail.com>
2022-10-29 13:24:33 +0100

    Implemented second order solver

--- eulers_method/eulers_method.py
+++ eulers_method/eulers_method.py
@@ -29,6 +29,20 @@ def euler_method(function:Callable,initial_values:list,endpoint:float,num_steps:
         y_values.append(new_y)
     return x_intervals,y_values
 
+def second_order_euler(function:Callable,position_initial_values:list,velocity_initial_value:float,endpoint:float,num_steps:int,) -> tuple:
+    x_0,y_0 = position_initial_values
+    v_0 = velocity_initial_value
+    step_size = (endpoint - x_0) / num_steps
+    x_intervals = np.arange(x_0,endpoint,step_size)
+    y_values = [y_0]
+    v_values = [v_0]
+    for i in range(1,num_steps):
+        new_y = y_values[i-1] + v_values[i-1] * step_size
+        new_v = v_values[i-1] + function(x_intervals[i-1],y_values[i-1]) * step_size
+        y_values.append(new_y)
+        v_values.append(new_v)
+    return x_intervals,y_values
+
 
 def euler_method_error(solution:Callable,function:Callable,initial_values:list,endpoint:float,num_steps:int,) -> tuple:
     """Will calculate the error at each step of the euler method vs the true solution.
@@ -57,4 +71,21 @@ def euler_method_error(solution:Callable,function:Callable,initial_values:list,e
     return x_range,error
 
 
-
+import matplotlib.pyplot as plt
+if __name__ ==  "__main__":
+    x_0 = 1
+    k = 1
+    m = 1
+    def RHS(t,x,k,m):
+        return -k*x/m
+    
+    x,y = second_order_euler(
+        function = lambda t,x: RHS(t,x,k,m),
+        position_initial_values=[0,x_0],
+        velocity_initial_value=0,
+        endpoint = 10,
+        num_steps = 5_00
+    )
+    plt.scatter(x,y)
+    plt.plot(x,x_0 * np.cos(x))
+    plt.show()
